173761b4975453a35934e26e9a4f233204c11302
iosua-pop <pop.iosua@yahoo.com>
2025-03-01 21:24:54 +0200

    laborator 11

---
+++ Maui+WebApi/AppMaui/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppMaui.Data;
+
+namespace AppMaui
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
